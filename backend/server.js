4ae8c29441e3b972966a001837cd5a5fe44f36ac
Nguyen Tung Lam <lamnguyentuung@gmail.com>
2024-04-28 20:09:48 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
@@ -12,6 +13,7 @@ dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -28,6 +30,17 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+//http://localhost:5000 => backend,frontend
+//http://localhost:3000 =>
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
